709f66901262cfeecf8001b9d8a144c05c9820e7
Juliana Alvares <alvares.juliana@hotmail.com>
2023-04-14 14:52:19 -0300

    requirement 5 - implement 'check_recipe_availability' and 'consume_recipe' methods from InventoryMapping class

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -27,8 +27,21 @@ class InventoryMapping:
 
     # Req 5.1
     def check_recipe_availability(self, recipe: Recipe):
-        pass
+        for ingredient, amount in recipe.items():
+            if (
+                ingredient not in self.inventory
+                or amount > self.inventory[ingredient]
+            ):
+                return False
+
+        return True
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        if not self.check_recipe_availability(recipe):
+            raise ValueError("Recipe is not available for consumption")
+
+        for ingredient, amount in recipe.items():
+            self.inventory[ingredient] -= amount
+            if self.inventory[ingredient] <= 0:
+                del self.Inventory[ingredient]
